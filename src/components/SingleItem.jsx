7ba90c798490b1a8ff22a0b7b308b3465cf9cef8
luiznaiper <luiz_naiper@hotmail.com>
2023-04-11 06:09:00 -0300

    :construction: Setup singleItem check item if isChecked

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,5 +1,28 @@
-const SingleItem = () => {
-  return <h2>SingleItem</h2>
+import { useState } from 'react'
+
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed)
+
+  const handleChange = () => {
+    setIsChecked(!isChecked)
+  }
+
+  return (
+    <div className="single-item">
+      <input type="checkbox" checked={isChecked} onChange={handleChange} />
+      <p
+        style={{
+          textTransform: 'capitalize',
+          textDecoration: isChecked && 'line-through',
+        }}
+      >
+        {item.name}
+      </p>
+      <button className="btn remove-btn" type="button">
+        Delete
+      </button>
+    </div>
+  )
 }
 
 export default SingleItem
